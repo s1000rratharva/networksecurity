8dc8cae7e8e5b16510d5cbe75495f7854da667ed
Atharva Keluskar <keluskaratharva999@gmail.com>
2026-08-03 17:29:52 +0530

    exception handling

---
+++ networkseurity/exception/exception.py
@@ -0,0 +1,19 @@
+import sys
+from networkseurity.logging import logger
+
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message, error_details:sys):
+        self.error_message= error_message
+        _,_,exc_tb=error_details.exc_info()
+
+        self.lineno=exc_tb.tb_lineno
+        self.file_name=exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return "Error message occured in [{0}] line number [{1}] error message [{3}]".format(
+            self.file_name,
+            self.lineno,
+            self.error_message
+        )
+
+
